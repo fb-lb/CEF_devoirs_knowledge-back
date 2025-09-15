f7004ba251e8ffaa126c083a774589c5630ad94a
fb.lb <fb.lubre@free.fr>
2025-09-15 16:54:37 +0200

    Set connection to the database in app.js

--- app.js
+++ app.js
@@ -4,20 +4,45 @@ import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
+import { Sequelize } from 'sequelize';
 
 import { router as indexRouter } from './routes/index.js';
 import { router as usersRouter } from './routes/users.js';
 
+// Get public folder path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// ------------------------------------
+// Initiate connection to the database
+// ------------------------------------
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: console.log,
+})
+
+// Connection test
+try {
+    await sequelize.authenticate();
+    console.log('Connected to the database.');
+} catch (error) {
+    console.error('Connection to the database failed : ', error);
+}
+
+// -----------------------
+// Express configuration
+// -----------------------
 const app = express();
 
+// Authorize front-end url to make request to the back-end
 app.use(cors({
     origin: process.env.FRONT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
